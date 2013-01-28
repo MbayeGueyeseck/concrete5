9eb66780377848373627c92350ff093d7a5096e4
Mbaye Gueye Seck <seck172@hotmail.fr>
2013-01-28 13:24:31 +0100

    concrete5
    Signed-off-by: Mbaye Gueye Seck <seck172@hotmail.fr>

---
+++ config/site.php
@@ -0,0 +1,8 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'concrete5');
+define('BASE_URL', 'http://localhost');
+define('DIR_REL', '/concrete5.4.1');
+define('PASSWORD_SALT', 'z9sEVGJjJaWXESgrUkYlMpT0UiAVhB4JzJcu8vpS0w3G2h6Q6RkF43Ltn9x0aL6H');
